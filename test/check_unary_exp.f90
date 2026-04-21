89ac4d290a181bfc3208c63e45f8b2ff92e420e4
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-04-22 06:50:44 +0900

    Squashed commit of the following:
    commit 2040ccec5d488deb073308cf06ae4bdd514b9478
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Wed Apr 22 06:47:46 2026 +0900
    
        Squashed commit of the following:
    
        commit 320d5c36de2e77b962e82337b1e25685c8666bc7
        Author: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
        Date:   Tue Apr 21 21:46:07 2026 +0000
    
            Auto-generate .f90 files from fypp [skip ci]
    
        commit ea6b9d40b6eaf4f306180abd7f8eb1e15b06ef3a
        Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
        Date:   Wed Apr 22 06:45:26 2026 +0900
    
            ci: fix targets for auto-commit fypp-generated files after successful tests
    
    commit 35863c5e0b1de1bb30899de41a0563ae4ad725dd
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Wed Apr 22 06:41:41 2026 +0900
    
        ci: update step to run fypp (test, neg)
    
    commit feeccc941e3e565fddaedff0a016b0f30a0480e8
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Wed Apr 22 06:36:17 2026 +0900
    
        ci: auto-commit fypp-generated files after successful tests
        Add commit job that pushes generated .f90 files to repository after all tests pass.
        Includes [skip ci] in commit message to prevent infinite loops.
    
    commit 29b1a8c03dde633e94ba634ac0f6a2e615b25926
    Merge: 6e9215b 87ff826
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Wed Apr 22 00:41:39 2026 +0900
    
        Merge branch 'develop' into develop_ci

---
+++ test/check_unary_exp.f90
@@ -0,0 +1,101 @@
+program check_unary_exp
+    !! [SymbolicRegressionPackage/EML_toolkit/EmL_compiler/Test_C_math_h/run_unary_suite_c.py at master · VA00/SymbolicRegressionPackage](https://github.com/VA00/SymbolicRegressionPackage/blob/master/EML_toolkit/EmL_compiler/Test_C_math_h/run_unary_suite_c.py)
+
+
+
+    use, intrinsic :: ieee_arithmetic, only: ieee_is_nan
+
+    use, non_intrinsic :: check_unary_config
+
+    use, non_intrinsic :: eml_type_fortran
+
+
+
+    implicit none
+
+
+
+    logical :: flag
+
+    integer :: i, file_unit
+
+    type(unary_data_type) :: record, trial
+
+
+
+    open( &!
+        newunit = file_unit , &!
+        file    = 'test/check_unary_exp.dat' , &!
+        status  = 'replace' &!
+    )
+
+
+
+    flag = .false.
+
+    call record%initialize
+
+
+
+    do i = -32, 32
+
+        call trial%initialize
+
+        trial%r_x  = 0.125_real64 * i
+        trial%e_x  = trial%r_x
+
+        trial%r_op = exp(trial%r_x)
+        trial%e_op = exp(trial%e_x)
+
+
+
+        if ( ieee_is_nan(trial%e_op) ) then
+
+            call trial%display('exp')
+
+            error stop ': exp(x) @ eml is NaN.'
+
+        end if
+
+
+
+        call trial%eval_error
+
+
+
+        write(file_unit, *) &!
+        &         trial%r_x               , & !  1
+        &   real( trial%e_x             ) , & !  2
+        &   imag( trial%e_x             ) , & !  3
+        &         trial%r_op              , & !  4
+        &   real( trial%e_op            ) , & !  5
+        &   imag( trial%e_op            ) , & !  6
+        &   real( trial%error        () ) , & !  7
+        &   imag( trial%error        () ) , & !  8
+        &   real( trial%error_scaled () ) , & !  9
+        &   imag( trial%error_scaled () )     ! 10
+
+
+
+        if ( abs( record%error_scaled() ) .lt. abs( trial%error_scaled() ) ) then
+
+            flag   = .true.
+            record = trial
+    
+        end if
+
+    end do
+
+
+
+    if (flag) call record%display('exp')
+
+
+
+    close(file_unit)
+
+
+
+    print *, 'OK: exp'
+
+end program
