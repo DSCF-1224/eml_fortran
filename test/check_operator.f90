8de8eb833da21d65e4f4526586f01de70a4ef396
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-04-23 23:51:21 +0900

    test: add program to call `check_unary_*`

---
+++ test/check_operator.f90
@@ -0,0 +1,17 @@
+program check_operator
+
+    use, non_intrinsic :: unary_operator_checker
+
+
+
+    implicit none
+
+
+
+    call check_unary_exp
+
+    call check_unary_log
+
+    call check_unary_neg
+
+end program
