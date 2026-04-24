7d7ef5496bcf00440201dc1204d5a306fcd0e035
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-04-25 08:36:36 +0900

    test: move common components from `*_operator_checker_type` to `operator_checker_class`

--- test/operator_checker.f90
+++ test/operator_checker.f90
@@ -19,6 +19,103 @@ module operator_checker
 
 
     type, abstract :: operator_checker_class
+
+        real(real64) :: r_x, r_op
+
+        type(eml_real64_type) :: e_x, e_op
+
+        real(real64), private :: hidden_spacing
+
+        complex(real64), private :: hidden_error, hidden_error_scaled
+
+        contains
+
+        procedure, pass :: error
+        procedure, pass :: error_scaled
+
+        procedure, pass :: display_error
+        procedure, pass :: eval_error
+        procedure, pass :: initialize_error
+
     end type
+
+
+
+    contains
+
+
+
+    elemental complex(real64) function error(self)
+
+        class(operator_checker_class), intent(in) :: self
+
+        error = self%hidden_error
+
+    end function
+
+
+
+    elemental complex(real64) function error_scaled(self)
+
+        class(operator_checker_class), intent(in) :: self
+
+        error_scaled = self%hidden_error_scaled
+
+    end function
+
+
+
+    subroutine display_error(self, operation)
+
+        class(operator_checker_class), intent(in) :: self
+
+        character(*), intent(in) :: operation
+
+
+
+        print *, self%hidden_error%re        , '; error%re'
+        print *, self%hidden_error%im        , '; error%im'
+        print *, self%hidden_spacing         , ';            spacing( ', operation, '(x) )'
+        print *, self%hidden_error_scaled%re , '; error%re / spacing( ', operation, '(x) )'
+        print *, self%hidden_error_scaled%im , '; error%im / spacing( ', operation, '(x) )'
+
+    end subroutine
+
+
+
+    elemental subroutine eval_error(self)
+
+        class(operator_checker_class), intent(inout) :: self
+
+
+
+        self%hidden_error   = cmplx   (self%e_op) - self%r_op
+        self%hidden_spacing = spacing (self%r_op)
+
+
+
+        if ( abs(self%r_op) .gt. 0.0_real64 ) then
+
+            self%hidden_error_scaled = self%hidden_error / self%hidden_spacing
+
+        else
+
+            self%hidden_error_scaled = self%hidden_error
+
+        end if
+
+    end subroutine
+
+
+
+    elemental subroutine initialize_error(self)
+
+        class(operator_checker_class), intent(inout) :: self
+
+        self%hidden_spacing      = 0.0_real64
+        self%hidden_error        = 0.0_real64
+        self%hidden_error_scaled = 0.0_real64
+
+    end subroutine
 
 end module
