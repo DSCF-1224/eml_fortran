24c1cec5e600f0acbd64560dcf8a7d40d9ae081a
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-04-29 18:54:50 +0900

    Squashed commit of the following:
    commit ed2a791dec79020aa4115148abce258ae38c5394
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Wed Apr 29 18:51:51 2026 +0900
    
        test: update program to test operators
        added `check_binary_avg`
    
    commit 299096f2c3be23cc63653eeb69228b997667e51e
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Wed Apr 29 18:49:46 2026 +0900
    
        Squashed commit of the following:
    
        commit 5428c8c73bef0b2f477468d4d898cb20da715b6d
        Author: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
        Date:   Wed Apr 29 09:46:22 2026 +0000
    
            Auto-generate .f90 files from fypp [skip ci]
    
        commit 3420684d41cccc41fb958854ec78e8829f88bf21
        Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
        Date:   Wed Apr 29 18:45:29 2026 +0900
    
            ci: update step to run fypp (test, avg)
    
    commit 05c2acebbd9c63800eeadea3368202e75d3afb5b
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Wed Apr 29 18:38:23 2026 +0900
    
        test: update `check_binary_operator.fypp` to test `avg` operator
    
    commit caf75488088a999cf775048165ab7f35df91a38d
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Wed Apr 29 18:34:51 2026 +0900
    
        test: update `check_binary_common.fypp` to test `avg` operator
    
    commit 68324d0033c0df78a6143ca6ec115287a838de61
    Author: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Date:   Wed Apr 29 09:30:41 2026 +0000
    
        Auto-generate .f90 files from fypp [skip ci]
    
    commit 7bb02d72827273365dae81164a4fa50cd16df33a
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Wed Apr 29 18:29:54 2026 +0900
    
        test: update `binary_operator_checker.fypp` to test `avg` operator
    
    commit 5d1e15e0fefe943cdc0f846789739522e5d740ab
    Merge: baea740 0f04fa4
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Wed Apr 29 18:25:12 2026 +0900
    
        Merge branch 'develop' into develop_test

--- test/binary_operator_checker.f90
+++ test/binary_operator_checker.f90
@@ -27,6 +27,7 @@ module binary_operator_checker
     public :: binary_operator_checker_type
 
     public :: check_binary_add
+    public :: check_binary_avg
     public :: check_binary_div
     public :: check_binary_mul
     public :: check_binary_sub
@@ -51,6 +52,9 @@ module binary_operator_checker
     interface
 
         module subroutine check_binary_add
+        end subroutine
+
+        module subroutine check_binary_avg
         end subroutine
 
         module subroutine check_binary_div
